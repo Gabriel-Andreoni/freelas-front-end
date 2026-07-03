da7313c7f5540486290ec2a0914df1aa2f325cab
Gabriel-Androeni <gabriel.andreoni@hotmail.com>
2026-07-03 13:50:42 -0300

    Alteração nos atributos htmlFor das labels e alterção de placehoder no componente Contrato

--- app/components/Contracts/index.tsx
+++ app/components/Contracts/index.tsx
@@ -6,35 +6,39 @@ export function Contracts() {
                     <h3 className="px-2 py-2 text-sm text-[#6da7ec] bg-[#032042] rounded-lg">Contrato {crypto.randomUUID()}</h3>
                 </div>
 
-                <div className="flex flex-col text-[#c3c2b7]">
-                    <label htmlFor="freelancer-name">Nome</label>
-                    <input
-                        className="p-2 border rounded-lg outline-none"
-                        type="text"
-                        id="freelancer-name"
-                        placeholder="Gabriel Andreoni"
-                    />
+                <div>
+                    <div className="flex flex-col text-[#c3c2b7]">
+                        <label htmlFor="contract-date">Data do Contrato</label>
+                        <input
+                            className="p-2 border rounded-lg outline-none"
+                            type="date"
+                            id="contract-date"
+                        />
+                 </div>
                 </div>
 
                 <div className="flex gap-4">
                     <div className="w-full flex flex-col text-[#c3c2b7]">
-                        <label htmlFor="freelancer-email">Email</label>
+                        <label htmlFor="freelancer-email">Duração (horas)</label>
                         <input
                             className="p-2 border rounded-lg outline-none"
-                            type="text"
+                            type="number"
                             id="freelancer-email"
-                            placeholder="gabriel.andreoni@gmail.com"
+                            placeholder="160"
                         />
                     </div>
 
                     <div className="w-full flex flex-col text-[#c3c2b7]">
-                        <label htmlFor="freelancer-department">Departamento</label>
-                        <select className="w-full h-full border rounded-lg outline-none">
-                            <option className="bg-[#171717]">Selecione</option>
-                            <option className="bg-[#171717]" value="T.I">T.I</option>
-                            <option className="bg-[#171717]" value="Marketing">Marketing</option>
-                            <option className="bg-[#171717]" value="Finance">Finance</option>
-                        </select>
+                        <label htmlFor="contract-value-per-hour">Valor por Hora</label>
+                        <div className="relative">
+                            <span className="absolute left-3 top-2.5 text-[#c3c2b7]">R$</span>
+                            <input
+                                className="p-2 border rounded-lg outline-none"
+                                type="number"
+                                id="contract-value-per-hour"
+                                placeholder="120"
+                            />
+                        </div>
                     </div>
                 </div>
             </section>
